724770b017d4e4a4d1f9469466beca6cf6ef7c3c
parth0811 <YOUR_NO_REPLY_EMAIL>
2026-02-16 21:28:52 +0530

    Initial backend deployment setup

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://url-shortner-frontend-ruddy.vercel.app/",
   credentials: true
 }));
 
