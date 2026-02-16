5ecfcd10bee88e0ba234816f1323ab7876918f5b
parth0811 <YOUR_NO_REPLY_EMAIL>
2026-02-16 21:17:56 +0530

    Initial backend deployment setup

--- src/config/monogo.config.js
+++ src/config/monogo.config.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
-console.log(process.env.MONGO_URI);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI not defined in environment variables");
+    }
+
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error("MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
