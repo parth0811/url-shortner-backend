6539cbf3f457cdd9f59083a1a72e16a71b199464
parth0811 <YOUR_NO_REPLY_EMAIL>
2026-02-17 12:52:59 +0530

    Fix cross-site cookie settings

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ dotenv.config("./.env")
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', // your React app
+    origin: 'https://url-shortner-frontend-ruddy.vercel.app', // your React app
     credentials: true // 👈 this allows cookies to be sent
 }));
 
